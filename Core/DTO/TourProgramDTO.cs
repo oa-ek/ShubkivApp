adb076a516704e33f4caa60c154e1005fcdba9c9
DmytroDolzhenko <dolzenkodima14@gmail.com>
2025-05-07 21:31:19 +0300

    upd

---
+++ Core/DTO/TourProgramDTO.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Core.DTO
+{
+    public class TourProgramViewModels
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<DayViewModels> Days { get; set; } = new();
+    }
+
+    public class DayViewModels
+    {
+        public int Id { get; set; }
+        public int DayNumber { get; set; }
+        public List<EventViewModels> Events { get; set; } = new();
+    }
+
+    public class EventViewModels
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public TimeOnly Time { get; set; }
+        public LocationViewModels Location { get; set; } = new();
+    }
+
+    public class LocationViewModels
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+}
--- WEBAPI/Models/DTO/EventDTO.cs
+++ WEBAPI/Models/DTO/EventDTO.cs
@@ -3,14 +3,15 @@ namespace WebApi.Models.DTO
 {
     public class EventDTO
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public TimeOnly Time { get; set; }
         public Location Location { get; set; }
         public int LocationId { get; set; }
-        public int DayId { get; set; }
-        public Day Day { get; set; }
-        public string ImageFilePath { get; set; }
+        //public int DayId { get; set; }
+        //public Day Day { get; set; }
+        //public string ImageFilePath { get; set; }
 
     }
 }
