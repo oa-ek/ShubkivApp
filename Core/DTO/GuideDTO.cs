2d5d6cb688fb7ce4b5c5a472f0dd13cfdac8effb
Andrii Zlotenko <workkanto@gmail.com>
2025-05-07 15:58:09 +0300

    Add new in guide

---
+++ Core/DTO/GuideDTO.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Core.DTO
+{
+    public class GuideDTOCreate
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Ім'я обов'язкове")]
+        public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Спеціальність обов'язкова")]
+        public string Specialty { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Контакт обов'язковий")]
+        public string Contact { get; set; } = string.Empty;
+    }
+
+    public class GuideViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Specialty { get; set; } = string.Empty;
+        public string Contact { get; set; } = string.Empty;
+    }
+}
